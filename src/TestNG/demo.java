44580f045810420097bc898cd7e1725df4e3bdfa
vipul <vipulalsundkar620@gmail.com>
2025-02-11 23:49:46 +0530

    develop1 commited for add new class

---
+++ src/TestNG/demo.java
@@ -0,0 +1,12 @@
+package TestNG;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	System.out.println("this is git commaand for push from github and pull code from github ");
+	}
+
+}
